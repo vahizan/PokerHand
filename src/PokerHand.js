d65fa9daa5dd83416a09fb5eb9a16633c501c670
Vahizan <vahizanv@gmail.com>
2018-10-10 20:17:04 +0100

    Testing edge cases

--- src/PokerHand.js
+++ src/PokerHand.js
@@ -1,9 +1,32 @@
 export class PokerHand {
-
+    constructor(pokerHand){
+        this.pokerHand = pokerHand;
+    }
 	compareWith() {
 		return Result.TIE;
 	}
 
+	maxCard(){
+	    return 0;
+    }
+
+    maxOccurrance(){
+	    return 0;
+    }
+
+    maxConsec(){
+        return 0;
+    }
+
+    validate(pokerHand){
+        //const pattern = /([\w]{2}\s{1}){4}([\w]{2})$/gmi;
+        const pattern = /^([T,J,Q,K,A,2,3,4,5,6,7,8,9]{1}[S,H,D,C]{1}\s{1}){4}([T,J,Q,K,A,2,3,4,5,6,7,8,9]{1}[S,H,D,C]{1})$/gmi;
+        console.log("pokerHand",pokerHand);
+        if(pattern.test(pokerHand)) {
+            return;
+        }
+        throw new Error("Please provide a valid input");
+    }
 }
 
 export const Result = {
--- src/PokerHand.test.js
+++ src/PokerHand.test.js
@@ -2,6 +2,31 @@ import PokerHand, { Result } from './PokerHand.js';
 
 describe('PokerHand', () => {
 
+    describe("handling incorrect input",()=>{
+        let handOne;
+        let handTwo;
+        beforeEach(()=>{
+            handOne = new PokerHand(null);
+            handTwo = new PokerHand(null);
+        });
+        test("throws Error on null input",()=>{
+            expect(()=>{handOne.validate(null)}).toThrowError(Error);
+            expect(()=>{handOne.validate(null)}).toThrowError(/^Please provide a valid input$/);
+        });
+        test("throws Error on empty string",()=>{
+            expect(()=>{handOne.validate("")}).toThrowError(Error);
+            expect(()=>{handOne.validate("")}).toThrowError(/^Please provide a valid input$/);
+        });
+        test("doesn't throw Error on valid input",()=>{
+            expect(handOne.validate("AC 4S 5S 8C AH")).toBe(undefined);
+        });
+        test("throw Error on valid input, but invalid characters",()=>{
+            expect(()=>{handOne.validate("LC 4M KS NQ AH")}).toThrowError(Error);
+            expect(()=>{handOne.validate("")}).toThrowError(/^Please provide a valid input$/);
+        });
+
+    });
+
 	describe('compareWith()', () => {
 
 		it(`ties`, () => {
