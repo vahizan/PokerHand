215e1d5c3f7b224290218092fce2e5227d55a95e
Vahizan <vahizanv@gmail.com>
2018-10-15 15:51:50 +0100

    Writing tests and implementation for analysing users hand

--- src/PokerHand.js
+++ src/PokerHand.js
@@ -1,34 +1,149 @@
 export class PokerHand {
     constructor(pokerHand){
         this.pokerHand = pokerHand;
+        this.cardValues = {};
+        this.valueList=[];
     }
 	compareWith() {
 		return Result.TIE;
 	}
 
-	maxCard(){
-	    return 0;
+	//Find highest value card
+	maxCard(cardValues){
+        this.validateArray(cardValues);
+        let maxVal = 0;
+        cardValues.forEach((val,i)=>{
+            maxVal = (val>maxVal)? val:maxVal;
+        });
+        return maxVal;
     }
 
-    maxOccurrance(){
-	    return 0;
+    //Find max number of pairs
+    maxPair(cardMap){
+        this.validateMap(cardMap);
+        let max =0;
+        for(let key in cardMap){
+            if (cardMap.hasOwnProperty(key)) {
+                let val = cardMap[key];
+                while(val>0){
+                    if(val%2 === 0) max++;
+                    val-=2;
+                }
+            }
+        }
+        return max;
     }
 
-    maxConsec(){
-        return 0;
+    maxOccurrance(cardMap){
+        this.validateMap(cardMap);
+        let max = 0;
+        for(let key in cardMap)
+            if(cardMap.hasOwnProperty(key)) max = (cardMap[key]>max)? cardMap[key]:max;
+	    return max;
     }
 
+    //if function returns 5 = straight
+    //14,2 === consecutive
+
+    maxConsec(cardValues){
+        this.validateArray(cardValues);
+        console.log("valuesSSSS",cardValues);
+        let ccount = 0;
+        if(cardValues[0]===LOWEST_CARD && cardValues[cardValues.length-1]===HIGHEST_CARD) ccount++;
+        for(let i =0;i<cardValues.length;i++){
+            if(i+1>=cardValues.length) break;
+            if(Math.abs(cardValues[i]-cardValues[i+1]) === 1) {
+                if(ccount === 0) ccount+=1;
+                ccount++;
+            }
+        }
+        console.log("COUNT ",ccount);
+        return ccount;
+    }
+
+    //checks if all values are consecutive from ten to ace
+    tenToAce(cardValues){
+        this.validateArray(cardValues);
+        return this.maxConsec(cardValues) === 5
+            && cardValues[0]===CARD_10
+            && cardValues[cardValues.length-1] === HIGHEST_CARD;
+    }
+
+    isRoyalFlush(cardValues,suitMap){
+        this.validateArray(cardValues);
+        this.validateMap(suitMap);
+        return this.tenToAce(cardValues) && this.maxOccurrance(suitMap)===5;
+    }
+
+    processHand(pokerHand){
+        pokerHand = pokerHand.toUpperCase();
+        return {
+            cardMap: this.cardMap(pokerHand),
+            cardValues: this.cardNumbers(pokerHand),
+            suits: this.suitMap(pokerHand)
+        }
+    }
+
+    toMap(pokerHand,index){
+        index = (index >= 1)?1:0;
+        let map =(index>0)?{"S":0,"C":0,"H":0,"D":0}:{};
+        this.validate(pokerHand);
+        let hands = pokerHand.split(" ");
+        for(let i in hands){
+            let key = hands[i].charAt(index);
+            if(!map[key]) map[key] = 1;
+            else map[key] += 1;
+        }
+        return map;
+    }
+    suitMap(pokerHand){return this.toMap(pokerHand,1);}
+
+    cardNumbers(pokerHand){
+        let cards = pokerHand.split(" ");
+        let values = [];
+        for(let i in cards){
+            let val = cards[i].charAt(0);
+            if(!Number.isInteger(val)) val = PokerCardValues[val];
+            values.push(parseInt(val));
+        }
+        values = values.sort((a,b)=> a-b);
+        return values;
+    }
+
+    cardMap(pokerHand){return this.toMap(pokerHand,0);}
+
+    containsDuplicate(strArr){
+        let map = {};
+        for(let i in strArr){
+            if(!map[strArr[i]]) map[strArr[i]] = 1;
+            else{
+                if(map[strArr[i]]+1===2) return true;
+                map[strArr[i]]+=1;
+            }
+        }
+        return false;
+    }
+    validateMap(cardMap){
+        if  (!cardMap || !(cardMap instanceof Object) || Object.keys(cardMap).length === 0
+            || Array.isArray(cardMap) ) throw new Error("Could not calculate the maximum number of pairs for this hand");
+    }
+    validateArray(cardValues){
+        if(!cardValues || !(cardValues instanceof Array) || cardValues.length===0) throw new Error("Could not calculate the maximum card value for this hand");
+    }
     validate(pokerHand){
         //const pattern = /([\w]{2}\s{1}){4}([\w]{2})$/gmi;
         const pattern = /^([T,J,Q,K,A,2,3,4,5,6,7,8,9]{1}[S,H,D,C]{1}\s{1}){4}([T,J,Q,K,A,2,3,4,5,6,7,8,9]{1}[S,H,D,C]{1})$/gmi;
-        console.log("pokerHand",pokerHand);
-        if(pattern.test(pokerHand)) {
+        if(pokerHand && pokerHand.length>0 && pattern.test(pokerHand.trim()) && !this.containsDuplicate(pokerHand.split(" "))){
             return;
         }
         throw new Error("Please provide a valid input");
     }
 }
 
+export const PokerCardValues = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"T":10,"J":11,"Q":12,"K":13,"A":14}
+export const LOWEST_CARD = 2;
+export const HIGHEST_CARD =14;
+export const CARD_10 =10;
 export const Result = {
 	WIN: 1,
 	LOSS: 2,
--- src/PokerHand.test.js
+++ src/PokerHand.test.js
@@ -1,8 +1,8 @@
-import PokerHand, { Result } from './PokerHand.js';
+import PokerHand, { Result,PokerCardValues } from './PokerHand.js';
 
 describe('PokerHand', () => {
 
-    describe("handling incorrect input",()=>{
+    describe("Handling incorrect input",()=>{
         let handOne;
         let handTwo;
         beforeEach(()=>{
@@ -20,14 +20,361 @@ describe('PokerHand', () => {
         test("doesn't throw Error on valid input",()=>{
             expect(handOne.validate("AC 4S 5S 8C AH")).toBe(undefined);
         });
+        test("doesn't throw Error on valid mixed case input",()=>{
+            expect(handOne.validate("Ac 4S 5s 8c ah")).toBe(undefined);
+        });
         test("throw Error on valid input, but invalid characters",()=>{
             expect(()=>{handOne.validate("LC 4M KS NQ AH")}).toThrowError(Error);
+            expect(()=>{handOne.validate("")}).toThrowError(/^Please provide a valid input$/);
+        });
+        test("throw Error on long invalid characters",()=>{
+            expect(()=>{handOne.validate("LC 4M KS NQ AH 9I pL 2A SD AS")}).toThrowError(Error);
+            expect(()=>{handOne.validate("")}).toThrowError(/^Please provide a valid input$/);
+        });
+
+        test("throw Error on invalid format",()=>{
+            expect(()=>{handOne.validate("LC sdssas4M KS sdasNQsdasAHdasds9I pL 2A")}).toThrowError(Error);
+            expect(()=>{handOne.validate("")}).toThrowError(/^Please provide a valid input$/);
+        });
+
+        //boolean - duplicate values ;
+        test("containsDuplicate - return true if duplicate values exist ", ()=>{
+            expect(handOne.containsDuplicate(["4S", "5S" ,"8C","AS", "AS"])).toBeTruthy();
+        });
+
+        test("containsDuplicate - return false if duplicate values doesn't exist ", ()=>{
+            expect(handOne.containsDuplicate(["4S", "5S" ,"8C","AD", "AS"])).toBeFalsy();
+        });
+
+        test("containsDuplicate - throw Error on duplicate values ",()=>{
+            expect(()=>{handOne.validate("4S 5S AA AA AA")}).toThrowError(Error);
             expect(()=>{handOne.validate("")}).toThrowError(/^Please provide a valid input$/);
         });
 
     });
 
-	describe('compareWith()', () => {
+    describe("Processing the input data - user's hand",()=>{
+        let handOne;
+
+        beforeEach(()=>{
+             handOne = new PokerHand(null);
+        });
+
+        test("should contain all card values", ()=>{
+            let cardValues = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"T":10,"J":11,"Q":12,"K":13,"A":14};
+            expect(cardValues).toEqual(PokerCardValues);
+        });
+
+        test("toMap - should return an object",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            expect(handOne.toMap('4S 5S 8C AS AD') instanceof Object).toBeTruthy();
+        });
+
+        test("toMap - should return only process poker hand",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            expect(()=>{handOne.toMap('4S 5S 8C AS AD 9D')}).toThrowError(Error);
+        });
+
+        test("toMap - should create a map of card values with index 0",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let outputOne = {"4":1,"5":1,"8":1,"A":2};
+            expect(handOne.toMap('4S 5S 8C AS AD',0)).toEqual(outputOne);
+        });
+
+        test("toMap - should create a map of suit values with index 1",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let outputOne = {"S":3,"C":1,"H":0,"D":1};
+            expect(handOne.toMap('4S 5S 8C AS AD',1)).toEqual(outputOne);
+        });
+
+        test("toMap - should create a map of suit values with index more than 1",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let outputOne = {"S":3,"C":1,"H":0,"D":1};
+            expect(handOne.toMap('4S 5S 8C AS AD',1000)).toEqual(outputOne);
+        });
+        test("toMap - should create a map of card values with index less than 0",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let outputOne = {"4":1,"5":1,"8":1,"A":2};
+            expect(handOne.toMap('4S 5S 8C AS AD',-1000)).toEqual(outputOne);
+        });
+
+        //Tests for suitMap function
+        test("suitMap - should return an object",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            expect(handOne.suitMap('4S 5S 8C AS AD') instanceof Object).toBeTruthy();
+        });
+
+        test("suitMap - should return an object with 4 properties. ",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let map = handOne.suitMap('4S 5S 8C AS AD');
+            let count =0;
+            for(let i in map) count++;
+            expect(count).toEqual(4);
+        });
+
+        test("suitMap - should return an object with 4 properties. Each property in following order S,C,H,D ",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let map = handOne.suitMap('4S 5S 8C AS AD');
+            let expectedKeys= ["S","C","H","D"];
+            let count =0;
+            for(let key in map) {
+                expect(key).toEqual(expectedKeys[count]);
+                count++;
+            }
+        });
+
+        test("suitMap - should return valid suit value data", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let suitM =  {"S":3,"C":1,"H":0,"D":1};
+            expect(handOne.suitMap('4S 5S 8C AS AD')).toEqual(suitM);
+        });
+
+
+        //Tests for cardMap function
+        test("cardMap - should return an object",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            expect(handOne.cardMap('4S 5S 8C AS AD') instanceof Object).toBeTruthy();
+        });
+
+        test("cardMap - should return an object with 4 properties. ",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let map = handOne.cardMap('4S 5S 8C AS AD');
+            let count =0;
+            for(let i in map) count++;
+            expect(count).toEqual(4);
+        });
+
+        test("cardMap - should return an object with 5 properties. ",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let map = handOne.cardMap('4S 5S 8C 3S AD');
+            let count =0;
+            for(let i in map) count++;
+            expect(count).toEqual(5);
+        });
+        test("cardMap - should return an object with 5 properties. Each property must be a number or A,T,J,Q,K ",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let map = handOne.cardMap('4S 5S 8C 3S AD');
+            let count =0;
+            for(let key in map) {
+                let patt = /[2,3,4,5,6,8,9,A,T,J,Q,K]/gmi;
+                expect(key.match(patt)).toBeTruthy();
+                count++;
+            }
+        });
+
+        test("cardMap - should return an object with 4 properties. Each property must be a number or A,T,J,Q,K ",()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let map = handOne.cardMap('4S 5S 8C AS AD');
+            let count =0;
+            for(let key in map) {
+                let patt = /[2,3,4,5,6,8,9,A,T,J,Q,K]/gmi;
+                expect(key.match(patt)).toBeTruthy();
+                count++;
+            }
+        });
+
+        test("cardMap - must return valid map", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let map = {"4":1,"5":1,"8":1,"A":2};
+            expect(handOne.cardMap('4S 5S 8C AS AD')).toEqual(map);
+        });
+
+        //Tests for cardNumbers function
+        test("cardValues - should return array", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let values = handOne.cardNumbers("4S 5S 8C AS AD");
+            expect(values instanceof Array).toBeTruthy();
+        });
+        test("cardValues - array should have 5 elements in total", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let actualValues = handOne.cardNumbers('4S 5S 8C AS AD');
+            expect(actualValues.length === 5).toBeTruthy();
+        });
+
+        test("cardValues - should store values as Number in array", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let expectedValues = [4,5,8,14,14];
+            let actualValues = handOne.cardNumbers('4S 5S 8C AS AD');
+            expect(actualValues.length === 5).toBeTruthy();
+            for(let i in actualValues) expect(Number.isInteger(actualValues[i])).toBeTruthy();
+        });
+
+        test("cardValues - should contain numbers only", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let expectedValues = [4,5,8,14,14];
+            let actualValues = handOne.cardNumbers('4S 5S 8C AS AD');
+            expect(actualValues).toEqual(expectedValues);
+
+        });
+
+        test("cardValues - should be sorted", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let expectedValues = [4,5,8,14,14];
+            let actualValues = handOne.cardNumbers('AS 8C 5H AC 4S');
+            expect(actualValues).toEqual(expectedValues);
+        });
+
+        //Tests for processHand
+        test("processHand - process valid hand - valid hand suit, card values and suits info", ()=>{
+            handOne = new PokerHand('4S 5S 8C AS AD');
+            let processedHand = {
+                cardMap: {"4":1,"5":1,"8":1,"A":2},
+                cardValues:[4,5,8,14,14],
+                suits: {"S":3,"C":1,"H":0,"D":1}
+            };
+            expect(handOne.processHand('4S 5S 8C AS AD')).toEqual(processedHand);
+        });
+    });
+
+    describe("Analysing user's hand",()=> {
+        let handOne;
+        beforeEach(() => {
+            handOne = new PokerHand(null);
+        });
+        //maxValue
+        test("should throw error for incorrect type param ",()=>{
+            expect(()=>{handOne.maxCard(null)}).toThrowError(Error);
+        });
+        test("should throw error for incorrect type param2 ",()=>{
+            expect(()=>{handOne.maxCard("hello")}).toThrowError(Error);
+        });
+        test("should throw error for empty array param ",()=>{
+            expect(()=>{handOne.maxCard([])}).toThrowError(Error);
+        });
+        test("should throw error for empty object param ",()=>{
+            expect(()=>{handOne.maxCard({})}).toThrowError(Error);
+        });
+        test("should throw error for  object param ",()=>{
+            expect(()=>{handOne.maxCard({A:2,B:3,X:6})}).toThrowError(Error);
+        });
+        test("should return the value of the highest card",()=>{
+            let processed = handOne.processHand("AC 4S 5S 8C AH");
+            expect(handOne.maxCard(processed.cardValues)).toBe(14);
+        });
+
+        //maxPair
+        test("should throw error for incorrect type param - null ",()=>{
+            expect(()=>{handOne.maxPair(null)}).toThrowError(Error);
+        });
+        test("should throw error for incorrect type param - string ",()=>{
+            expect(()=>{handOne.maxPair("hello")}).toThrowError(Error);
+        });
+        test("should throw error for incorrect type param - empty array ",()=>{
+            expect(()=>{handOne.maxPair([])}).toThrowError(Error);
+        });
+        test("should throw error for empty object param",()=>{
+            expect(()=>{handOne.maxPair({})}).toThrowError(Error);
+        });
+        test("should return the value max number of pairs - 2 Pairs",()=>{
+            let processed = handOne.processHand("AC 4S 5S 8C AH");
+            console.log("processed",processed);
+            expect(handOne.maxPair(processed.cardMap)).toBe(1);
+        });
+        test("should return the value of max number of pairs - 4 of a kind",()=>{
+            let processed = handOne.processHand("AC AS AD 8C AH");
+            console.log("processed",processed);
+            expect(handOne.maxPair(processed.cardMap)).toBe(2);
+        });
+        test("should return the value of max number of pairs - two different pairs",()=>{
+            let processed = handOne.processHand("8D AS AD 8C 7H");
+            console.log("processed",processed);
+            expect(handOne.maxPair(processed.cardMap)).toBe(2);
+        });
+
+        //max occurrance
+
+        test("maxOccurrance - should throw error for incorrect type param - null ",()=>{
+            expect(()=>{handOne.maxOccurrance(null)}).toThrowError(Error);
+        });
+        test("maxOccurrance - should throw error for incorrect type param - string ",()=>{
+            expect(()=>{handOne.maxOccurrance("hello")}).toThrowError(Error);
+        });
+        test("maxOccurrance - should throw error for incorrect type param - empty array ",()=>{
+            expect(()=>{handOne.maxOccurrance([])}).toThrowError(Error);
+        });
+        test("maxOccurrance - should throw error for empty object param",()=>{
+            expect(()=>{handOne.maxOccurrance({})}).toThrowError(Error);
+        });
+
+        test("maxOccurrance - should return max no of occurrances of a value - One Pair",()=>{
+            let processed = handOne.processHand("AC 4S 5S 8C AH");
+            console.log("processed",processed);
+            expect(handOne.maxOccurrance(processed.cardMap)).toBe(2);
+        });
+        test("maxOccurrance - should return the value of max number of occurrances - 4 of a kind",()=>{
+            let processed = handOne.processHand("AC AS AD 8C AH");
+            console.log("processed",processed);
+            expect(handOne.maxOccurrance(processed.cardMap)).toBe(4);
+        });
+        test("should return the value of max number of occurrances - two different pairs",()=>{
+            let processed = handOne.processHand("8D AS AD 8C 7H");
+            console.log("processed",processed);
+            expect(handOne.maxOccurrance(processed.cardMap)).toBe(2);
+        });
+
+        //max consec
+        test("maxConsec - should return max no of consecutive values - 0 consecutive numbers",()=>{
+            let processed = handOne.processHand("AC 4S 8S 8C AH");
+            expect(handOne.maxConsec(processed.cardValues)).toBe(0);
+        });
+        test("maxConsec - should return max no of consecutive values - 2 consecutive numbers",()=>{
+            let processed = handOne.processHand("AC 9S 2S 8C AH");
+            expect(handOne.maxConsec(processed.cardValues)).toBe(2);
+        });
+        test("maxConsec - should return max no of consecutive values - 4 consecutive numbers",()=>{
+            let processed = handOne.processHand("KC QS AD JC 7H");
+            expect(handOne.maxConsec(processed.cardValues)).toBe(4);
+        });
+        test("maxConsec - should return max no of consecutive values - straight",()=>{
+            let processed = handOne.processHand("KC QS AD TC JH");
+            expect(handOne.maxConsec(processed.cardValues)).toBe(5);
+        });
+        test("maxConsec - should return max no of consecutive values - 3 consecutive values",()=>{
+            let processed = handOne.processHand("KC 2S AD TC JH");
+            expect(handOne.maxConsec(processed.cardValues)).toBe(3);
+        });
+        test("maxConsec - should return max no of consecutive values - straight 2",()=>{
+            let processed = handOne.processHand("KC QH AD TC JH");
+            expect(handOne.maxConsec(processed.cardValues)).toBe(5);
+        });
+
+
+
+        //tenToAce
+        test("tenToAce - should return false if all values are not consec",()=>{
+            let processed = handOne.processHand("AC 4S 8S 8C AH");
+            expect(handOne.tenToAce(processed.cardValues)).toBe(false);
+        });
+        test("tenToAce - should return true - straight and between 10 to A",()=>{
+            let processed = handOne.processHand("QC TS KS JC AH");
+            expect(handOne.tenToAce(processed.cardValues)).toBe(true);
+        });
+        test("tenToAce - should return false - straight, but not between 10 to A",()=>{
+            let processed = handOne.processHand("2c 5s 4S 6C 3H");
+            expect(handOne.tenToAce(processed.cardValues)).toBe(false);
+        });
+
+
+        //isRoyalFlush
+
+        test("isRoyalFlush - should return false as values aren't consec",()=>{
+            let processed = handOne.processHand("AC 4S 8S 8C AH");
+            expect(handOne.isRoyalFlush(processed.cardValues,processed.suits)).toBe(false);
+        });
+        test("isRoyalFlush - should return false - straight, between 10 to A, but not all in same suit",()=>{
+            let processed = handOne.processHand("QC TS KS JC AH");
+            expect(handOne.isRoyalFlush(processed.cardValues,processed.suits)).toBe(false);
+        });
+        test("isRoyalFlush - should return false - straight, but not between 10 to A",()=>{
+            let processed = handOne.processHand("Qh th jh ah kH");
+            expect(handOne.isRoyalFlush(processed.cardValues,processed.suits)).toBe(true);
+        });
+
+
+
+    });
+
+        describe('compareWith()', () => {
 
 		it(`ties`, () => {
 
@@ -37,6 +384,8 @@ describe('PokerHand', () => {
 			expect(hand1.compareWith(hand2)).toBe(Result.TIE);
 
 		});
+
+
 
 	});
 
